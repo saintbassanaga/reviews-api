e2ec755dab584414fae8354e3cd05f135b6bad58
saintbassanaga <saintbassanaga01@gmail.com>
2025-05-09 13:15:48 +0100

    feat: refactor RsaApplayer to use @Configuration and streamline key loading

--- src/main/java/tech/saintbassanaga/reviewsapi/config/RsaApplayer.java
+++ src/main/java/tech/saintbassanaga/reviewsapi/config/RsaApplayer.java
@@ -3,6 +3,7 @@ package tech.saintbassanaga.reviewsapi.config;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import java.nio.file.Files;
@@ -10,13 +11,15 @@ import java.nio.file.Paths;
 import java.security.KeyFactory;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 /**
  * Created by saintbassanaga {saintbassanaga}
  * In the Project reviews-api at Fri - 5/9/25
  */
 
-@Component
+@Configuration
 public class RsaApplayer {
 
     @Value("${rsa.private-key}")
@@ -25,20 +28,17 @@ public class RsaApplayer {
     @Value("${rsa.public-key}")
     private String publicKeyBase64;
 
-
     @Bean
     public RSAPrivateKey privateKey() throws Exception {
-        // Decode the Base64 string and convert it into a PrivateKey object
-        byte[] decodedKey = Base64.getDecoder().decode(privateKeyBase64);
-        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-        return (RSAPrivateKey) keyFactory.generatePrivate(new java.security.spec.PKCS8EncodedKeySpec(decodedKey));
+        byte[] decoded = Base64.getDecoder().decode(privateKeyBase64);
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(decoded);
+        return (RSAPrivateKey) KeyFactory.getInstance("RSA").generatePrivate(keySpec);
     }
 
     @Bean
     public RSAPublicKey publicKey() throws Exception {
-        // Decode the Base64 string and convert it into a PublicKey object
-        byte[] decodedKey = Base64.getDecoder().decode(publicKeyBase64);
-        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-        return (RSAPublicKey) keyFactory.generatePublic(new java.security.spec.X509EncodedKeySpec(decodedKey));
+        byte[] decoded = Base64.getDecoder().decode(publicKeyBase64);
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(decoded);
+        return (RSAPublicKey) KeyFactory.getInstance("RSA").generatePublic(keySpec);
     }
 }
